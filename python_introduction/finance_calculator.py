f365b777ba0d4cf88f7f24dc83b1726130a8319a
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-07-10 17:22:16 +0300

    Fix variable names for ALX checker

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,19 +1,10 @@
-# Let's help the user with money calculations!
 
-# Ask them how much money they earn every month
-income = float(input("Enter your monthly income: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-# Ask them how much they spend
-expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 
-# Monthly savings = income - expenses
-monthly_savings = income - expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-# Projected savings in a year = 12 months of saving + 5% interest
-yearly_savings = monthly_savings * 12
-interest = yearly_savings * 0.05
-projected_savings = yearly_savings + interest
-
-# Show them the results!
 print("Your monthly savings are $" + str(monthly_savings) + ".")
 print("Projected savings after one year, with interest, is: $" + str(projected_savings) + ".")
