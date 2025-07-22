cc59145a7224da6d1d11aa1e8904fce77ae561c6
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-07-22 22:31:44 +0300

    Add arithmetic operations and test file

---
+++ fns_and_dsa/test_operations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from arithmetic_operations import add, subtract, multiply, divide
+
+a = 20
+b = 5
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
+print("Multiplication:", multiply(a, b))
+print("Division:", divide(a, b))
