e685402fb54b3b67439eacf34f8f77521ee3ab20
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-07-22 22:45:38 +0300

    Fix arithmetic_operations and add test_arithmetic_operations script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
-
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    return a / b
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Cannot divide by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
