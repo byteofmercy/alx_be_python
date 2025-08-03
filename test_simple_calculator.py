085f4ed0bdb700e047c5c180a124fbdbfed4efc6
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-08-03 14:40:39 +0300

    Add simple calculator with CLI and unit tests

--- test_simple_calculator.py
+++ test_simple_calculator.py
@@ -1,20 +1,24 @@
 import unittest
-from simple_calculator import add, subtract, multiply, divide
+from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
 
-    def test_add(self):
-        self.assertEqual(add(10, 5), 15)
+    def setUp(self):
+        self.calc = SimpleCalculator()
 
-    def test_subtract(self):
-        self.assertEqual(subtract(10, 5), 5)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(10, 5), 15)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
 
     def test_multiply(self):
-        self.assertEqual(multiply(10, 5), 50)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
 
     def test_divide(self):
-        self.assertEqual(divide(10, 2), 5)
-        self.assertRaises(ZeroDivisionError, divide, 10, 0)
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
