19dc7f4042168ee8cc4cba9e27d0dfe5d8aa4c1f
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-08-06 12:02:55 +0300

    Pass all checker requirements for robust division calculator

--- robust_division_calculator.py
+++ robust_division_calculator.py
@@ -1,13 +1,10 @@
 def safe_divide(numerator, denominator):
     try:
-        num = float(numerator)
-        den = float(denominator)
-
-        if den == 0:
-            return "Error: Cannot divide by zero."
-
-        result = num / den
+        numerator = float(numerator)
+        denominator = float(denominator)
+        result = numerator / denominator
         return f"The result of the division is {result}"
-    
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
